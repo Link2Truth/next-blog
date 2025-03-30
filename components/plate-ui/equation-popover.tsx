630b81e5ddd8fbb723833dbff797565a5063e63f
RK Xu <xvrongkang@outlook.com>
2025-03-30 11:16:25 +0800

    style: 调整公式块输入框大小

--- components/plate-ui/equation-popover.tsx
+++ components/plate-ui/equation-popover.tsx
@@ -1,25 +1,24 @@
-'use client';
+"use client";
 
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import TextareaAutosize, {
   type TextareaAutosizeProps,
-} from 'react-textarea-autosize';
+} from "react-textarea-autosize";
 
-import type { TEquationElement } from '@udecode/plate-math';
+import type { TEquationElement } from "@udecode/plate-math";
 
-import { cn } from '@udecode/cn';
-import { useEquationInput } from '@udecode/plate-math/react';
-import { BlockSelectionPlugin } from '@udecode/plate-selection/react';
+import { cn } from "@udecode/cn";
+import { useEquationInput } from "@udecode/plate-math/react";
+import { BlockSelectionPlugin } from "@udecode/plate-selection/react";
 import {
   createPrimitiveComponent,
   useEditorRef,
   useElement,
   useReadOnly,
-} from '@udecode/plate/react';
-import { CornerDownLeftIcon } from 'lucide-react';
+} from "@udecode/plate/react";
 
-import { Button } from './button';
-import { PopoverContent } from './popover';
+import { Button } from "./button";
+import { PopoverContent } from "./popover";
 
 const EquationInput = createPrimitiveComponent(TextareaAutosize)({
   propsHook: useEquationInput,
@@ -62,22 +61,18 @@ const EquationPopoverContent = ({
 
   return (
     <PopoverContent
-      className="flex gap-2"
+      className="flex gap-2 min-w-[40vw]"
       onEscapeKeyDown={(e) => {
         e.preventDefault();
       }}
       contentEditable={false}
     >
       <EquationInput
-        className={cn('max-h-[50vh] grow resize-none p-2 text-sm', className)}
+        className={cn("max-h-[50vh]  grow resize-none p-2 text-sm", className)}
         state={{ isInline, open, onClose }}
         autoFocus
         {...props}
       />
-
-      <Button variant="secondary" className="px-3" onClick={onClose}>
-        Done <CornerDownLeftIcon className="size-3.5" />
-      </Button>
     </PopoverContent>
   );
 };
