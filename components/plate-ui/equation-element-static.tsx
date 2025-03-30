438de19852cdd11a86c9d10fea36731005760470
RK Xu <xvrongkang@outlook.com>
2025-03-30 11:16:48 +0800

    style: 汉化

--- components/plate-ui/equation-element-static.tsx
+++ components/plate-ui/equation-element-static.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import type { TEquationElement } from '@udecode/plate-math';
+import type { TEquationElement } from "@udecode/plate-math";
 
-import { cn } from '@udecode/cn';
-import { type SlateElementProps, SlateElement } from '@udecode/plate';
-import { getEquationHtml } from '@udecode/plate-math';
-import { RadicalIcon } from 'lucide-react';
+import { cn } from "@udecode/cn";
+import { type SlateElementProps, SlateElement } from "@udecode/plate";
+import { getEquationHtml } from "@udecode/plate-math";
+import { RadicalIcon } from "lucide-react";
 
 export function EquationElementStatic({
   children,
@@ -18,23 +18,23 @@ export function EquationElementStatic({
     element,
     options: {
       displayMode: true,
-      errorColor: '#cc0000',
+      errorColor: "#cc0000",
       fleqn: false,
       leqno: false,
-      macros: { '\\f': '#1f(#2)' },
-      output: 'htmlAndMathml',
-      strict: 'warn',
+      macros: { "\\f": "#1f(#2)" },
+      output: "htmlAndMathml",
+      strict: "warn",
       throwOnError: false,
       trust: false,
     },
   });
 
   return (
-    <SlateElement className={cn('my-1', className)} {...props}>
+    <SlateElement className={cn("my-1", className)} {...props}>
       <div
         className={cn(
-          'group flex items-center justify-center rounded-sm select-none hover:bg-primary/10 data-[selected=true]:bg-primary/10',
-          element.texExpression.length === 0 ? 'bg-muted p-3 pr-9' : 'px-2 py-1'
+          "group flex items-center justify-center rounded-sm select-none hover:bg-primary/10 data-[selected=true]:bg-primary/10",
+          element.texExpression.length === 0 ? "bg-muted p-3 pr-9" : "px-2 py-1"
         )}
       >
         {element.texExpression.length > 0 ? (
@@ -46,7 +46,7 @@ export function EquationElementStatic({
         ) : (
           <div className="flex h-7 w-full items-center gap-2 text-sm whitespace-nowrap text-muted-foreground">
             <RadicalIcon className="size-6 text-muted-foreground/80" />
-            <div>Add a Tex equation</div>
+            <div>添加Tex公式</div>
           </div>
         )}
       </div>
--- components/plate-ui/equation-element.tsx
+++ components/plate-ui/equation-element.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 
-import type { TEquationElement } from '@udecode/plate-math';
+import type { TEquationElement } from "@udecode/plate-math";
 
-import { cn, withRef } from '@udecode/cn';
-import { useEquationElement } from '@udecode/plate-math/react';
-import { PlateElement, useElement, useSelected } from '@udecode/plate/react';
-import { RadicalIcon } from 'lucide-react';
+import { cn, withRef } from "@udecode/cn";
+import { useEquationElement } from "@udecode/plate-math/react";
+import { PlateElement, useElement, useSelected } from "@udecode/plate/react";
+import { RadicalIcon } from "lucide-react";
 
-import { EquationPopoverContent } from './equation-popover';
-import { Popover, PopoverTrigger } from './popover';
+import { EquationPopoverContent } from "./equation-popover";
+import { Popover, PopoverTrigger } from "./popover";
 
 export const EquationElement = withRef<typeof PlateElement>(
   ({ children, className, ...props }, ref) => {
@@ -25,27 +25,27 @@ export const EquationElement = withRef<typeof PlateElement>(
       katexRef: katexRef,
       options: {
         displayMode: true,
-        errorColor: '#cc0000',
+        errorColor: "#cc0000",
         fleqn: false,
         leqno: false,
-        macros: { '\\f': '#1f(#2)' },
-        output: 'htmlAndMathml',
-        strict: 'warn',
+        macros: { "\\f": "#1f(#2)" },
+        output: "htmlAndMathml",
+        strict: "warn",
         throwOnError: false,
         trust: false,
       },
     });
 
     return (
-      <PlateElement ref={ref} className={cn('my-1', className)} {...props}>
+      <PlateElement ref={ref} className={cn("my-1", className)} {...props}>
         <Popover open={open} onOpenChange={setOpen} modal={false}>
           <PopoverTrigger asChild>
             <div
               className={cn(
-                'group flex cursor-pointer items-center justify-center rounded-sm select-none hover:bg-primary/10 data-[selected=true]:bg-primary/10',
+                "group flex cursor-pointer items-center justify-center rounded-sm select-none hover:bg-primary/10 data-[selected=true]:bg-primary/10",
                 element.texExpression.length === 0
-                  ? 'bg-muted p-3 pr-9'
-                  : 'px-2 py-1'
+                  ? "bg-muted p-3 pr-9"
+                  : "px-2 py-1"
               )}
               data-selected={selected}
               contentEditable={false}
@@ -56,7 +56,7 @@ export const EquationElement = withRef<typeof PlateElement>(
               ) : (
                 <div className="flex h-7 w-full items-center gap-2 text-sm whitespace-nowrap text-muted-foreground">
                   <RadicalIcon className="size-6 text-muted-foreground/80" />
-                  <div>Add a Tex equation</div>
+                  <div>添加Tex公式</div>
                 </div>
               )}
             </div>
@@ -64,7 +64,7 @@ export const EquationElement = withRef<typeof PlateElement>(
 
           <EquationPopoverContent
             open={open}
-            placeholder={`f(x) = \\begin{cases}\n  x^2, &\\quad x > 0 \\\\\n  0, &\\quad x = 0 \\\\\n  -x^2, &\\quad x < 0\n\\end{cases}`}
+            placeholder={`f(x) = \\begin{cases}  x^2, &\\quad x > 0 \\\\  0, &\\quad x = 0 \\\\  -x^2, &\\quad x < 0\\end{cases}`}
             isInline={false}
             setOpen={setOpen}
           />
